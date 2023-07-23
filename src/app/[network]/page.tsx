2d110007f51d8e8b8be6ea2a316d73538de819ad
Nando <24811313+fzavalia@users.noreply.github.com>
2023-07-23 01:15:41 -0300

    fix: Remove util

--- src/app/[network]/page.tsx
+++ src/app/[network]/page.tsx
@@ -3,14 +3,24 @@ import Image from "next/image";
 import NextLink from "next/link";
 import { Button, Container, Link, Paper, Table, TableBody, TableCell, TableContainer, TableRow } from "@mui/material";
 import classNames from "classnames";
-import { explorerLinks, fetchContracts } from "@/utils/fetchContracts";
-import logo from "./img/logo.png";
 import styles from "./page.module.css";
+
+export type NetworkContracts = Record<string, string>;
+
+export type Contracts = Record<string, NetworkContracts>;
 
 export type NetworkPageProps = {
   params: {
     network: string;
   };
+};
+
+export const explorers: Record<string, string> = {
+  mainnet: "https://etherscan.io",
+  goerli: "https://goerli.etherscan.io",
+  sepolia: "https://sepolia.etherscan.io",
+  matic: "https://polygonscan.com",
+  mumbai: "https://mumbai.polygonscan.com",
 };
 
 export default async function NetworkPage({ params }: NetworkPageProps) {
@@ -57,7 +67,7 @@ export default async function NetworkPage({ params }: NetworkPageProps) {
                     {row.name}
                   </TableCell>
                   <TableCell sx={{ fontSize: "1rem", fontFamily: "monospace" }} align="right">
-                    <Link href={`${explorerLinks[currentNetwork]}/address/${row.address}`}>{row.address}</Link>
+                    <Link href={`${explorers[currentNetwork]}/address/${row.address}`}>{row.address}</Link>
                   </TableCell>
                 </TableRow>
               ))}
@@ -66,5 +76,18 @@ export default async function NetworkPage({ params }: NetworkPageProps) {
         </TableContainer>
       </main>
     </Container>
+  );
+}
+
+async function fetchContracts(): Promise<Contracts> {
+  const response = await fetch("https://contracts.decentraland.org/addresses.json");
+
+  const contracts: Contracts = await response.json();
+
+  const wl = new Set(["mainnet", "goerli", "sepolia", "matic", "mumbai"]);
+
+  return Object.entries(contracts).reduce(
+    (acc, [network, contracts]) => (wl.has(network) ? { ...acc, [network]: contracts } : acc),
+    {} as Contracts
   );
 }
